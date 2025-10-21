0ffd13aee1fcfc94b36d9c7d0e0c0d6a31dc2756
Yaroslav Kuksin <71522584+ya-kksn@users.noreply.github.com>
2025-10-21 11:13:57 +0300

    Update update_domains.sh
    Change docker-compose to docker compose
    Change download link

--- update_domains.sh
+++ update_domains.sh
@@ -1,4 +1,4 @@
-docker-compose down
-curl -L -o "proxy.txt" "https://github.com/ImMALWARE/dns.malw.link/raw/refs/heads/master/domains.txt"
-curl -L -o "proxy_with_subdomains.txt" "https://github.com/ImMALWARE/dns.malw.link/raw/refs/heads/master/domains_with_subdomains.txt"
-docker-compose up -d --build
+docker compose down
+curl -L -o "proxy.txt" "https://github.com/ya-kksn/dns.malw.link/raw/refs/heads/master/domains.txt"
+curl -L -o "proxy_with_subdomains.txt" "https://github.com/ya-kksn/dns.malw.link/raw/refs/heads/master/domains_with_subdomains.txt"
+docker compose up -d --build
